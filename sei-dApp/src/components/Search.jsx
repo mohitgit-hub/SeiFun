03c213a71552c75d366dc3e662f3dc08f537dde3
PRAVESH DHAKAL <pravdhakal110@gmail.com>
2025-04-13 19:54:03 +0530

    second commit

--- sei-dApp/src/components/Search.jsx
+++ sei-dApp/src/components/Search.jsx
@@ -11,9 +11,9 @@ export default function Search({ searchItem, setSearchItem }) {
 				Search
 			</label>
 			<div class='relative'>
-				<div class='absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none'>
+				<div className='absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none'>
 					<svg
-						class='w-4 h-4 text-gray-500 dark:text-gray-400'
+						className='w-4 h-4 text-gray-500 dark:text-gray-400'
 						aria-hidden='true'
 						xmlns='http://www.w3.org/2000/svg'
 						fill='none'
@@ -31,7 +31,7 @@ export default function Search({ searchItem, setSearchItem }) {
 				<input
 					type='search'
 					id='default-search'
-					class='block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+					className='block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
 					placeholder='Search your favorite meme coins, etc'
 					required
 					value={searchItem}
@@ -39,7 +39,7 @@ export default function Search({ searchItem, setSearchItem }) {
 				/>
 				<button
 					type='submit'
-					class='text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
+					className='text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
 				>
 					Search
 				</button>
